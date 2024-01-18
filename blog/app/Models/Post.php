ccaa2baa472d85d5106b53cde12a8077d087ee32
Jordan Turner <turnerjordan855@gmail.com>
2024-01-18 09:12:29 +0000

    episode 22

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -1,66 +1,19 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
 
-class Post
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    public function __construct(public $title, public $excerpt, public $date, public $body, public $slug)
-    {
-        $this->title = $title;
+    use HasFactory;
 
-        $this->excerpt = $excerpt;
-
-        $this->date = $date;
-
-        $this->body = $body;
-
-        $this->slug = $slug;
-    }
-
-
-    public static function all()
-    {
-        return cache()->rememberForever('posts.all', function(){ // cache the results of this method forever. Give it a key of posts.all that can be refereneced later on to clear the cache
-
-            return collect(File::files(resource_path("posts"))) // create a collection of files
-            ->map(fn($file) => YamlFrontMatter::parseFile($file)) // map over the collection of files and parse each file as a YamlFrontMatter object
-            ->map(fn($document) => // map over the collection of YamlFrontMatter objects and create a new Post object from each. The end result is a collection of Post objects
-        
-                new Post(
-                    $document->title,
-                    $document->excerpt,
-                    $document->date,
-                    $document->body(),
-                    $document->slug
-                )
-            )
-            ->sortByDesc('date'); 
-        });
-    }
-
-    public static function find($slug)
-    {
-        // all() method returns a collection of Post objects
-        // firstWhere() method returns the first Post object that matches the slug
-        // if no matching post is found, null is returned
-
-        return static::all()->firstWhere('slug', $slug);
-    }
-
-    public static function findOrFail($slug)
-    {
-        // defer to the find() method to find the post
-
-        $post = static::find($slug);
-
-        if(! $post) // if no post is found, throw an exception
-        {
-            throw new ModelNotFoundException();
-        }
-
-        return $post; // if a post is found, return it
-    }
+    // the fillable property is used to specify which attributes can be mass assigned
+    protected $fillable =
+    [
+        'title',
+        'excerpt',
+        'body'
+    ];
 }
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -25,25 +25,38 @@ Route::get('/', function ()
 
 });
 
-// curly brackets are used to indicate a dynamic route (wildcard)
-// we can then pass the wildcard as a variable to the function
-Route::get('posts/{post}', function($slug)
-{
-    // find a post by its slug and pass it to a view called "post"
 
-    /* 
-        you can pass a second argument to the view. The find method on the Post class will return the data for the requested post, 
-        which allows us to create a variable called $post and make it available on the post view
-    */
-    
-    /*
-        we could possibly use something similar for courses in the VT system. Whenever a user is visits course/something we could 
-        use the something as a wildcard and then use the something to find the course in the database and then pass the course to the view
-    */
+// using Eloquent to find a post by its id
+Route::get('posts/{post}', function($id)
+{
+    // find a post by its id and pass it to a view called "post"
 
     return view('post', [
-        'post' => Post::findOrFail($slug) //find post matching the slug or throw an exception (404)
+        'post' => Post::findOrFail($id) //find post matching the id or throw an exception (404)
         ]
     );
 
-});
+})->where('post', '[0-9]+'); // only match if the post is a number
+
+// curly brackets are used to indicate a dynamic route (wildcard)
+// we can then pass the wildcard as a variable to the function
+// Route::get('posts/{post}', function($slug)
+// {
+//     // find a post by its slug and pass it to a view called "post"
+
+//     /* 
+//         you can pass a second argument to the view. The find method on the Post class will return the data for the requested post, 
+//         which allows us to create a variable called $post and make it available on the post view
+//     */
+    
+//     /*
+//         we could possibly use something similar for courses in the VT system. Whenever a user is visits course/something we could 
+//         use the something as a wildcard and then use the something to find the course in the database and then pass the course to the view
+//     */
+
+//     return view('post', [
+//         'post' => Post::findOrFail($slug) //find post matching the slug or throw an exception (404)
+//         ]
+//     );
+
+// });
